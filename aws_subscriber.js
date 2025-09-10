961db3eb540a90aa632fc3a2f8359006ab8ee53f
kushan malintha <e20243@eng.pdn.ac.lk>
2025-09-10 21:04:46 +0530

    exhibit

--- aws_subscriber.js
+++ aws_subscriber.js
@@ -3,10 +3,10 @@ const awsIot = require("aws-iot-device-sdk");
 // AWS IoT Device Configuration
 const device = awsIot.device({
   keyPath:
-    "./cert_exhibit/1c0e76c62d58f716b82ae99a257860aa55b29e4aea5470b92c3eefcea8daae3b-private.pem.key",
+    "./cert/1c0e76c62d58f716b82ae99a257860aa55b29e4aea5470b92c3eefcea8daae3b-private.pem.key",
   certPath:
-    "./cert_exhibit/1c0e76c62d58f716b82ae99a257860aa55b29e4aea5470b92c3eefcea8daae3b-certificate.pem.crt",
-  caPath: "./cert_exhibit/AmazonRootCA1.pem",
+    "./cert/1c0e76c62d58f716b82ae99a257860aa55b29e4aea5470b92c3eefcea8daae3b-certificate.pem.crt",
+  caPath: "./cert/AmazonRootCA1.pem",
   clientId: `myNodeSubscriber-${Date.now()}`,
   host: "a2xhp106oe6s98-ats.iot.ap-southeast-2.amazonaws.com",
 });
